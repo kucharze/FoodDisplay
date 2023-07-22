54991d065364b92378b230315ce91ec0c202f8a4
Zachary Kuchar <zacharyk323@live.com>
2023-07-22 11:59:37 -0400

    Initial setup with new Api

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -3,28 +3,38 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
 function Food() {
-
-    
+   
       const foodList = [
         { name: "Chicken", symbol: "chicken" },
-        { name: "Lit", symbol: "LIT" },
-        { name: "Eth", symbol: "ETH" },
-        { name: "Et", symbol: "ET" },
         { name: "Steak", symbol: "Steak" },
         { name: "Dash", symbol: "DASH" },
     ];
+    const [food,setFood] = useState(null)
+   
  
-    const getFood = async () =>{
-        //This link does not work
-        // let food = await fetch(
-        //     "www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
-        //     )
-        // let response = await food.json()
+    const url = 'https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser?nutrition-type=cooking&category%5B0%5D=generic-foods';
+// const url = "http://www.edamam.com/ontologies/edamam.owl#Food_01001"
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e8015e2cf4msh97855d9c2558c02p122c3ajsnaafe58f99606',
+            'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+        }
+    };
 
-        // console.log(response)
+    const getItem = async () =>{
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            // console.log(result.hints);
+            setFood(result.hints)
+        } catch (error) {
+            console.error(error);
+        }
     }
+
     useEffect(()=>{
-        getFood()
+        getItem()
     },[])
 
   return (
@@ -32,8 +42,10 @@ function Food() {
       <h1>The food page</h1>
       <h1>Hello</h1>
       {
-        foodList.map((item,i)=>{
-            return <Link to={`/ingrediants/:${item.symbol}`}><h2>{item.name}</h2></Link>
+        food && 
+        food.map((item,i)=>{
+          console.log(item.food)
+            return <Link key={i} to={`/ingrediants/:${item.food}`}><h2>{item.food.label}</h2></Link>
         })
       }
     </div>
--- src/pages/Ingrediants.jsx
+++ src/pages/Ingrediants.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Ingrediants() {
     let item = useParams().item
+    const [food,setFood] = useState('')
+
+    // const url = 'https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser?nutrition-type=cooking&category%5B0%5D=generic-foods';
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-RapidAPI-Key': 'e8015e2cf4msh97855d9c2558c02p122c3ajsnaafe58f99606',
+    //         'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+    //     }
+    // };
+
+    // const getItem = async () =>{
+    //     try {
+    //         const response = await fetch(url, options);
+    //         const result = await response.json();
+    //         console.log(result.hints);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    // useEffect(()=>{
+    //     getItem()
+    // },[])
+
+     //https://api.github.com/users/kucharze
   return (
     <div>
       <h1>The Ingrediants</h1>
