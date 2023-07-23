0b36e064062637b4c4901a083564203bbb40b479
Zachary Kuchar <zacharyk323@live.com>
2023-07-23 12:13:41 -0400

    Added logo pic

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,6 +5,8 @@ function Main() {
     <div className='main'>
       <h1>Welcome to my plate!</h1>
       <h2>🍴 See our great recipes 🍴 </h2>
+      <img src='https://cdn2.f-cdn.com/contestentries/163701/3286727/54f0f507644bc_thumb900.jpg'
+      className='mainImg'/>
     </div>
   )
 }
